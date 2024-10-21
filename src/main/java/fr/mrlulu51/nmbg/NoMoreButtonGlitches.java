517a358457ae5971b692fdde292b4606362dbf3b
mrlulu51 <lulubinou51@gmail.com>
2024-10-21 17:58:57 +0200

    Initial Commit

---
+++ src/main/java/fr/mrlulu51/nmbg/NoMoreButtonGlitches.java
@@ -0,0 +1,11 @@
+package fr.mrlulu51.nmbg;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoMoreButtonGlitches implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Fr bro");
+    }
+}
